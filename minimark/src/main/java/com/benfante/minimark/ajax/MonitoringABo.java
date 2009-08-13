2acb405899df433293be85b673c9fd2abab7f1f8
lucio <devnull@localhost>
2009-08-13 14:18:33 +0000

    Added refresh buttons.

--- minimark/src/main/java/com/benfante/minimark/ajax/MonitoringABo.java
+++ minimark/src/main/java/com/benfante/minimark/ajax/MonitoringABo.java
@@ -3,11 +3,15 @@ package com.benfante.minimark.ajax;
 import com.benfante.minimark.blo.AssessmentFillingBo;
 import com.benfante.minimark.dao.AssessmentFillingDao;
 import com.benfante.minimark.po.AssessmentFilling;
+import com.benfante.minimark.po.Course;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import org.apache.log4j.Logger;
 import org.directwebremoting.Browser;
 import org.directwebremoting.ScriptSession;
 import org.directwebremoting.WebContext;
@@ -26,6 +30,7 @@ import org.springframework.stereotype.Service;
 @RemoteProxy(name = "monitoringABo")
 public class MonitoringABo {
 
+    private static final Logger logger = Logger.getLogger(MonitoringABo.class);
     @Resource
     private AssessmentFillingDao assessmentFillingDao;
     @Resource
@@ -33,54 +38,93 @@ public class MonitoringABo {
 
     @RemoteMethod
     public void confirmAssessmentFilling(Long assessmentFillingId,
-            final Long courseId) throws IOException, ServletException {
-        final WebContext wctx = WebContextFactory.get();
+            Long courseId) throws IOException, ServletException {
+        WebContext wctx = WebContextFactory.get();
         ScriptSession session = wctx.getScriptSession();
         AssessmentFilling filling =
                 assessmentFillingDao.get(assessmentFillingId);
         filling.setConfirmedDate(new Date());
         assessmentFillingDao.store(filling);
-        Browser.withSession(session.getId(), new Runnable() {
-
-            public void run() {
-                try {
-                    HttpServletRequest req = wctx.getHttpServletRequest();
-                    req.setAttribute("blockFillings", assessmentFillingBo.
-                            searchActiveFillingsByCourse(courseId));
-                    Util.setValue("course_"+courseId+"_fillings", wctx.forwardToString(
-                            "/WEB-INF/jsp/monitoring/fillingBlock.jsp"), false);
-                } catch (Exception ex) {
-                    Util.setValue("searchResult", ex.getMessage());
-                }
-            }
-        });
-
+        Browser.withSession(session.getId(), new BlockFillingsRefresher(wctx,
+                courseId));
     }
 
     @RemoteMethod
     public void unconfirmAssessmentFilling(Long assessmentFillingId,
-            final Long courseId) throws IOException, ServletException {
-        final WebContext wctx = WebContextFactory.get();
+            Long courseId) throws IOException, ServletException {
+        WebContext wctx = WebContextFactory.get();
         ScriptSession session = wctx.getScriptSession();
         AssessmentFilling filling =
                 assessmentFillingDao.get(assessmentFillingId);
         filling.setConfirmedDate(null);
         assessmentFillingDao.store(filling);
+        Browser.withSession(session.getId(), new BlockFillingsRefresher(wctx,
+                courseId));
+    }
+
+    @RemoteMethod
+    public void refreshFillingsBlock(Long courseId) throws IOException,
+            ServletException {
+        WebContext wctx = WebContextFactory.get();
+        ScriptSession session = wctx.getScriptSession();
+        Browser.withSession(session.getId(), new BlockFillingsRefresher(wctx,
+                courseId));
+    }
+
+    @RemoteMethod
+    public void refreshAllFillingsBlocks() throws IOException, ServletException {
+        final Map<Course, List<AssessmentFilling>> courseFillings =
+                assessmentFillingBo.mapFillingsOnCourse();
+        final WebContext wctx = WebContextFactory.get();
+        ScriptSession session = wctx.getScriptSession();
         Browser.withSession(session.getId(), new Runnable() {
 
             public void run() {
-                try {
-                    HttpServletRequest req = wctx.getHttpServletRequest();
-                    req.setAttribute("blockFillings", assessmentFillingBo.
-                            searchActiveFillingsByCourse(courseId));
-                    Util.setValue("course_"+courseId+"_fillings", wctx.forwardToString(
-                            "/WEB-INF/jsp/monitoring/fillingBlock.jsp"), false);
-                } catch (Exception ex) {
-                    Util.setValue("searchResult", ex.getMessage());
+                HttpServletRequest req = wctx.getHttpServletRequest();
+                for (Course course : courseFillings.keySet()) {
+                    try {
+                        req.setAttribute("blockFillings", courseFillings.get(
+                                course));
+                        Util.setValue("course_" + course.getId() + "_fillings", wctx.forwardToString(
+                                "/WEB-INF/jsp/monitoring/fillingBlock.jsp"),
+                                false);
+                    } catch (Exception ex) {
+                        logger.error(
+                                "Error refreshing the block fillings for course " + course.getId(),
+                                ex);
+                        Util.setValue("course_" + course.getId() + "_fillings",
+                                ex.getMessage());
+                    }
                 }
             }
         });
-
     }
 
+    private class BlockFillingsRefresher implements Runnable {
+
+        private WebContext wctx;
+        private Long courseId;
+
+        public BlockFillingsRefresher(WebContext wctx, Long courseId) {
+            this.wctx = wctx;
+            this.courseId = courseId;
+        }
+
+        public void run() {
+            try {
+                HttpServletRequest req = wctx.getHttpServletRequest();
+                req.setAttribute("blockFillings", assessmentFillingBo.
+                        searchActiveFillingsByCourse(courseId));
+                Util.setValue("course_" + courseId + "_fillings", wctx.
+                        forwardToString(
+                        "/WEB-INF/jsp/monitoring/fillingBlock.jsp"), false);
+            } catch (Exception ex) {
+                logger.error(
+                        "Error refreshing the block fillings for course " + this.courseId,
+                        ex);
+                Util.setValue("course_" + courseId + "_fillings",
+                        ex.getMessage());
+            }
+        }
+    }
 }
--- minimark/src/main/webapp/WEB-INF/jsp/monitoring/fillings.jsp
+++ minimark/src/main/webapp/WEB-INF/jsp/monitoring/fillings.jsp
@@ -1,5 +1,6 @@
 <%@ include file="/WEB-INF/jsp/common.jspf" %>
 <h3><spring:message code="AssessmentFillings" text="?AssessmentFillings?"/></h3>
+<a href="#" class="action-refresh" onclick="monitoringABo.refreshAllFillingsBlocks(); return false;"><spring:message code='RefreshAll' text='?RefreshAll?'/></a>
 <c:choose>
     <c:when test="${empty fillings}">
         <spring:message code="NoAssessmentFillings" text="?NoAssessmentFillings?"/>
@@ -7,7 +8,15 @@
     <c:otherwise>
         <c:forEach var="course" items="${fillings}">
             <div class="filling_course">
-                <h4>${course.key.name}<br/><span class="smallText">${course.key.incumbent}</span></h4>
+                <div class="filling_header">
+                    <div class="fh_title">
+                        ${course.key.name}<br/><span class="smallText">${course.key.incumbent}</span>
+                    </div>
+                    <div class="fh_actions">
+                        <a href="#" class="action-refresh" onclick="monitoringABo.refreshFillingsBlock(${course.key.id}); return false;" title="<spring:message code='Refresh' text='?Refresh?'/>"></a>
+                    </div>
+                </div>
+                <h4></h4>
                 <div id="course_${course.key.id}_fillings">
                     <c:set var="blockFillings" value="${course.value}"/>
                     <%@include file="fillingBlock.jsp" %>
