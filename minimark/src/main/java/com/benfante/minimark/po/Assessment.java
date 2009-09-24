03968a0594d8d1a289b0c139ada4453d93116b00
lucio.benfante <lucio dot benfante at gmail dot com>
2009-09-24 06:57:51 +0200

    Fixed assessment list order. Not it's by date,course,title.

--- minimark/src/main/java/com/benfante/minimark/po/Assessment.java
+++ minimark/src/main/java/com/benfante/minimark/po/Assessment.java
@@ -50,7 +50,7 @@ import org.springmodules.validation.bean.conf.loader.annotation.handler.NotNull;
 @NamedQueries({
     @NamedQuery(name = "Assessment.findByTeacherUsername",
     query =
-    "select a from Course c join c.assessments a join c.courseTeachers ct where ct.userProfile.user.username = ? order by a.assessmentDate desc")
+    "select a from Course c join c.assessments a join c.courseTeachers ct where ct.userProfile.user.username = ? order by a.assessmentDate desc, c.name asc, a.title asc")
 })
 public class Assessment extends EntityBase {
 
